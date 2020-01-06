88e0652081f2cbe76f94708fbccb7ff743fc6670
Bob Green <bob.green@madysoft.com>
2020-01-06 17:05:47 -0500

    Presentational and Container Components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import Directory from './components/DirectoryComponents';
-import {CAMPSITES} from './shared/campsites';
+import Main from './components/MainComponent';
 
 class App extends Component {
-    constructor(props){
-        super(props);
-        this.state = {campsites: CAMPSITES};
-    };
-
     render() {
         return ( 
-            <Directory campsites={this.state.campsites}/>
+            <div className="App">
+                <Main/>
+            </div>
         );
-    }
+    };
 }
 export default App
