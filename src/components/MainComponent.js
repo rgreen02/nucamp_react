c77bb5beea98c5f34283f55dd14dba8e872c3dae
Bob Green <bob.green@madysoft.com>
2020-01-15 00:19:02 -0500

    Contact Form

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,27 +4,59 @@ import Footer from './FooterComponent';
 import Directory from './DirectoryComponents';
 import CampsiteInfo from './CampsiteInfoComponent';
 import {CAMPSITES} from '../shared/campsites';
+import Home from './HomeComponent';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { PARTNERS } from '../shared/partners';
+import { Switch, Route, Redirect} from 'react-router-dom';
+import Contact from './ContactComponent';
+import About from './AboutComponent'; 
 
 class Main extends Component {
     constructor(props){
         super(props);
         this.state = {
             campsites: CAMPSITES,
-            selectedCampsite: null
+            comments: COMMENTS,
+            partners: PARTNERS,
+            promotions: PROMOTIONS
+            
         };
     }
 
-    onCampsiteSelect(campsiteId) {
-        this.setState({selectedCampsite: campsiteId});
-    }
-
     render() {
-        return(
+        
+        const HomePage = () => {
+            return (
+                <Home
+                campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
+                promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
+                partner={this.state.partners.filter(partner => partner.featured)[0]}
+                />
+            );
+        }
+
+        const CampsiteWithId = ({match}) => {
+            return(
+                <CampsiteInfo
+                campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+                />
+            );
+        }
+
+        return (
             <div>
                 <Header />
-                <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)}/>
-                <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]}/>
-                <Footer />
+                <Switch>
+                    <Route path='/home' component={HomePage} />
+                    <Route path='/directory/:campsiteId' component={CampsiteWithId} />
+                    <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} /> 
+                    <Route exact path='/aboutus' render={() => <About partners={this.state.partners} />} />
+                    <Route exact path='/contactus' component={Contact} />
+                    <Redirect to='/home' />
+                </Switch>
+                 <Footer />
             </div>
         );
     };//End of Render
